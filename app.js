4e7a53e285b94ffd965d162b48fc7eeca26a109a
ProKafelek12e <pro.kafelek@gmail.com>
2023-02-16 00:41:12 +0100

    Help button

--- app.js
+++ app.js
@@ -7,6 +7,27 @@ function send(){
     document.getElementById("chatArea").appendChild(p)
     resp(msg)
 }
+function help(){
+    //div to handle ig and msg from mango
+    const div = document.createElement("div")
+    div.classList.add("msg-div")
+    
+    //mango img before msg
+    const icon = document.createElement("img")
+    icon.src = "mango-64.png"
+    icon.classList.add("logo")
+    
+    //element for msg
+    const p = document.createElement("p")
+    p.innerHTML = "I can do..."
+    p.classList.add("twoje")
+    p.classList.add("msg")
+
+    div.appendChild(icon)
+    div.appendChild(p)
+    document.getElementById("chatArea").appendChild(div)
+
+}
 function resp(msg){
     //div to handle ig and msg from mango
     const div = document.createElement("div")
